d9617cb6619fcdc38cad255078c8c31e4c891ed5
Rohan Nair <r@rohannair.ca>
2018-08-09 12:51:06 -0400

    Removing need for connection string, and just constructing it

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,15 @@
 
 require('dotenv').config({ silent: true })
 
+const USER = process.env.POSTGRES_USER || 'postgres'
+const HOST = process.env.POSTGRES_HOST || '127.0.0.1'
+const DB = process.env.POSTGRES_DB
+const PORT = process.env.POSTGRES_PORT || '5432'
+const ssl = !!process.env.SSL
+
 module.exports = {
   client: 'postgresql',
-  connection: process.env.DB_CONNECTION_STRING,
+  connection: `postgresql://${USER}@${HOST}:${PORT}/${DB}${ssl && '?ssl=true'}`,
   pool: {
     min: 2,
     max: 10,
